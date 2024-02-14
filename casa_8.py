dd2e2c6118d8a4b22cb932c7dd15fef9c8c581f7
isabellaovalle08 <iovalle@miners.utep.edu>
2024-02-14 08:56:32 -0700

    Update casa_8.py

--- casa_8.py
+++ casa_8.py
@@ -1,1 +1,8 @@
+import pandas as pd 
+df=pd.read_csv('big-mac-full-index.csv')
+#method 4 iterrows() function DF
+for ind, row in df.iterrows():
+    print( row['currency_code'],row['date'] )
 
+#method 6 apply() function DF
+    print(df.apply(lambda row: (row['currency_code'], row['date']), axis=1))
